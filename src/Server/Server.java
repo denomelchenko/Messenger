57d83db7cf15783a344d5b282aa3304bc5026fca
Denys <denysomelchenko.dev@gmail.com>
2023-05-24 18:53:37 +0300

    update

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,7 +1,7 @@
 package Server;
 
 public class Server {
-    public static final short PORT = 999;
+    public static final short PORT = 8080;
 
     public static void main(String[] args) {
         new MsgServer(PORT).start();
